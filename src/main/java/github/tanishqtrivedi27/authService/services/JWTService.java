ec2452958862ee01f67b835443e325e548600336
Tanishq Trivedi <tanishq.trivedi27@gmail.com>
2024-10-24 21:22:17 +0530

    increased JWT expiration to 1 hour

--- src/main/java/github/tanishqtrivedi27/authService/services/JWTService.java
+++ src/main/java/github/tanishqtrivedi27/authService/services/JWTService.java
@@ -59,7 +59,7 @@ public class JWTService {
                 .claims(claims)
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
                 .signWith(key)
                 .compact();
     }
